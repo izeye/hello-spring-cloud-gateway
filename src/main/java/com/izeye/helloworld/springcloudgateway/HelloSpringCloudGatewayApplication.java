bacb4e1f853e85059ea3373da2ec166c282273ee
Johnny Lim <izeye@naver.com>
2023-02-01 19:59:32 +0900

    Use application properties

--- src/main/java/com/izeye/helloworld/springcloudgateway/HelloSpringCloudGatewayApplication.java
+++ src/main/java/com/izeye/helloworld/springcloudgateway/HelloSpringCloudGatewayApplication.java
@@ -2,9 +2,6 @@ package com.izeye.helloworld.springcloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 /**
  * Hello world for Spring Cloud Gateway.
@@ -13,15 +10,6 @@ import org.springframework.context.annotation.Bean;
  */
 @SpringBootApplication
 public class HelloSpringCloudGatewayApplication {
-
-	@Bean
-	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route(p -> p.path("/get")
-						.filters(f -> f.addRequestHeader("Hello", "World"))
-						.uri("https://httpbin.org"))
-				.build();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringCloudGatewayApplication.class, args);
